2424c3beeade3ff6a7271d5e288f3e11172f60fa
Martin <martinmontanari3@gmail.com>
2020-06-14 18:38:28 -0300

    Add init arq and mysql.sh

---
+++ resources/views/products.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if
+                <div>
+
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Hola mundo! Acá van a ir los productos.
+                </div>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
